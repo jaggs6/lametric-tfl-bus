fa74b2edb2f2e8d60843b9f8721a2d5ffe948d6f
Benjamin Caplan <benjamin.caplan@bskyb.com>
2017-02-23 16:47:37 +0000

    Remove sync-request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
+
 const http = require('http');
-const request = require('sync-request');
+const request = require('request');
 const sortBy = require('sort-by');
 
 const hostname = '0.0.0.0';
@@ -9,28 +11,28 @@ const server = http.createServer((req, res) => {
   if(req.url != '/favicon.ico') {
     console.log(req.url);
     const url = 'https://api.tfl.gov.uk/StopPoint' + req.url + '/Arrivals';
-    var resp = request('GET', url);
-    var response = JSON.parse(resp.getBody());
+    request(url, (err, response, body) => {
+      const json = JSON.parse(body);
+      let output = ' ';
+      json.sort(sortBy('timeToStation'));
+      json.forEach(function(bus) {
+        const time = bus.timeToStation;
+        const minutes = Math.floor(time / 60);
+        const seconds = time - minutes * 60;
+        output += minutes + ',';
+      });
+      output = output.slice(0, -1);
+      output = output.trim();
 
-    var output = ' ';
-    response.sort(sortBy('timeToStation'));
-    response.forEach(function(bus) {
-      var time = bus.timeToStation;
-      var minutes = Math.floor(time / 60);
-      var seconds = time - minutes * 60;
-      output += minutes + ',';
+      if(output === '') {
+        output = 'no buses';
+      } else {
+        output = json[0].lineName + ' in ' + output;
+      }
+      res.statusCode = response.statusCode;
+      res.setHeader('Content-Type', 'application/json');
+      res.end('{"frames": [{"text": "' + output + '","icon": "i996"}]}');
     });
-    output = output.slice(0, -1);
-    output = output.trim();
-
-    if(output === '') {
-      output = 'no buses';
-    } else {
-      output = response[0].lineName + ' in ' + output;
-    }
-    res.statusCode = resp.statusCode;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('{"frames": [{"text": "' + output + '","icon": "i996"}]}');
   }
 });
 
