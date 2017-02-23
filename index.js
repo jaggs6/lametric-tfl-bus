4b03674378422a09cb92e00762ecceb48160fb17
Jagdeep Nagpal <jagdeepnagpal@gmail.com>
2017-02-23 17:50:09 +0000

    add frames

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 const request = require('request');
-const sortBy = require('sort-by');
+const _ = require('underscore');
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -12,26 +12,30 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
     const url = 'https://api.tfl.gov.uk/StopPoint' + req.url + '/Arrivals';
     request(url, (err, response, body) => {
-      const json = JSON.parse(body);
+      let json = JSON.parse(body);
       let output = ' ';
-      json.sort(sortBy('timeToStation'));
+      json = _.sortBy(json, 'timeToStation');
+      const max = _.max(json, function(bus){ return bus.timeToStation; });
+      let goalFrames = '';
       json.forEach(function(bus) {
         const time = bus.timeToStation;
         const minutes = Math.floor(time / 60);
-        const seconds = time - minutes * 60;
-        output += minutes + ',';
+        const goalFrame = ',{"goalData":{"start":0,"current":'+minutes+',"end":'+Math.floor(max.timeToStation / 60)+',"unit":"mins"},"icon":null}';
+        goalFrames = goalFrames + goalFrame;
       });
-      output = output.slice(0, -1);
-      output = output.trim();
 
-      if(output === '') {
+      if(goalFrames === '') {
         output = 'no buses';
       } else {
-        output = json[0].lineName + ' in ' + output;
+        output = json[0].lineName + ' towards ' + json[0].towards;
       }
+      const firstFrame = '{"text":"'+output+'","icon":"i996"}';
+
+      const finalFrame = '{"frames":[' + firstFrame + goalFrames + ']}';
+
       res.statusCode = response.statusCode;
       res.setHeader('Content-Type', 'application/json');
-      res.end('{"frames": [{"text": "' + output + '","icon": "i996"}]}');
+      res.end(finalFrame);
     });
   }
 });
